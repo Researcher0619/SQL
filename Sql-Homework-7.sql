fa31a4a3f56e0ee0ce365016319eb24d8a0c573c
Özkan Ceyhan <102052449+Researcher0619@users.noreply.github.com>
2024-01-13 23:30:11 +0300

    Add files via upload

---
+++ Sql-Homework-7.sql
@@ -0,0 +1,25 @@
+-- Group the films in the film table according to their rating values.
+SELECT rating,COUNT(*)
+FROM film
+GROUP BY rating;
+
+-- When we group the films in the film table according to the replacement_cost column, sort the replacement_cost value and the corresponding number of films with more than 50 films.
+SELECT replacement_cost, COUNT(*)
+FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+
+-- What are the number of customers corresponding to the store_id values in the customer table? 
+SELECT store_id, COUNT(*)
+FROM customer
+GROUP BY store_id
+
+
+-- After grouping the city data in the city table according to the country_id column, share the country_id information and the number of cities with the highest number of cities.
+SELECT country_id, COUNT(*)
+FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
